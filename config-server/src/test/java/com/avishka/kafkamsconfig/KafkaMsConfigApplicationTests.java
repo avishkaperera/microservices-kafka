c16641db037121291c4990baeae4a60ab9bdc7fb
Avishka Perera <avishka@cygnus.lk>
2019-12-19 13:25:28 +0530

    Initial commit

---
+++ config-server/src/test/java/com/avishka/kafkamsconfig/KafkaMsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishka.kafkamsconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMsConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-registry/src/test/java/com/avishka/kafkamsregistry/KafkaMsRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishka.kafkamsregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMsRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
